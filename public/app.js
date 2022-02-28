2d8d7dda03bd7b7f2af7bf1174da2fbea1422e00
GiftBanda <bandagift42@gmail.com>
2022-02-28 15:35:43 +0200

    second commit

---
+++ public/app.js
@@ -0,0 +1,27 @@
+import { Invoice } from './classes/invoice.js';
+//classes
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'worked on the luigi website', 500);
+// console.log(invOne, invTwo);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
+// console.log(invoices);
+// const anchor = document.querySelector('a')!
+// // if(anchor) {
+// //     console.log(anchor.href)
+// // }
+// console.log(anchor.href)
+// const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// console.log(form.children)
+// const type = document.querySelector('#type') as HTMLSelectElement
+// const toFrom = document.querySelector('#tofrom') as HTMLInputElement
+// const details = document.querySelector('#details') as HTMLInputElement
+// const amount = document.querySelector('#amount') as HTMLInputElement
+// form.addEventListener('submit', (e: Event) => {
+//     e.preventDefault()
+//     console.log(type.value, toFrom.value, details.value, amount.valueAsNumber)
+// })
---
+++ src/sandbox.ts
@@ -0,0 +1,195 @@
+// let character = 'mario';
+// let age = 40;
+// let isBlackBelt = false;
+
+//won't work
+//character = 1;
+//age = 'twenty';
+//isBlackBelt = 'true';
+
+//will work
+// character = 'luigi';
+// age = 30;
+// isBlackBelt = true;
+
+//functions
+
+//  const circ = (diameter: number)=> {
+//     return diameter * Math.PI;
+// }
+
+// console.log(circ(2));
+
+//check how long it takes to run a function
+//console.time('circ');
+//console.timeEnd('circ');
+
+
+
+//Types basics 
+
+//Objects and arrays
+
+// let names = ['mario', 'luigi', 'yoshi'];
+// names.push('toad');
+// names.push(3);
+// names[1] = 40;
+
+// let numbers = [1, 2, 3];
+// numbers.push(4);
+// numbers.push('four');
+// numbers[1] = 'two';
+
+// let mixed = [1, 'two', true];
+// mixed.push('four');
+// mixed.push(false);
+// mixed.push(20);
+// mixed[1] = 6;
+
+//objects
+
+// let ninja = {
+//     name: 'mario',
+//     age: 40,
+//     belt: 'black',
+// };
+
+//ninja.age = 45;
+//ninja.skills = ['fighting', 'sneaking'];
+// ninja.name = 'yoshi';
+//ninja.age = '59';
+
+// ninja = {
+//     name: 'yoshi',
+//     age: 59,
+//     belt: 'green',
+//    // skills: ['sneaking', 'fighting'],
+// };
+
+//explicit types
+// let character: string;
+// let age: number;
+// let isLoggedIn: boolean;
+
+
+
+//arrays
+
+// let ninjas: string[] = [];
+
+// ninjas.push('Gift', 'Esther', 'Mario');
+
+//union types
+
+// let mixed: (string | number | boolean)[] = [];
+// mixed.push('luigi');
+// mixed.push(40);
+// mixed.push(true);
+
+// console.log(mixed);
+
+// let uid: string | number;
+// uid = '1234';
+// uid = 1234;
+
+//objects
+
+// let ninjaOne: object;
+
+// ninjaOne = { name: 'mario', age: 40 };
+
+// let ninjaTwo: { name: string; age: number; belt: string; };
+
+// ninjaTwo = {
+//     name: 'yoshi',
+//     age: 59,
+//     belt: 'green',
+// };
+
+// console.log(ninjaOne);
+
+//dynamic type
+
+// let age: any;
+
+// age = 40;
+// age = '40';
+// age = true;
+// age = {name: 'mario', age: 40};
+
+// let mixed: any[] = [];
+// mixed.push(40);
+// mixed.push('40');
+// mixed.push(true);
+// mixed.push({name: 'mario', age: 40});
+
+// let ninja: {name: any, age: any}
+// ninja = {name: 'mario', age: 40};
+
+// console.log('test')
+
+//functions type
+
+// let greet: Function;
+
+// greet = () => {
+//     console.log('hello');
+// }
+
+// console.log(greet());
+
+// let add = (a: number, b: number, c?: number | string) => {
+//     console.log(a + b);
+//     console.log(c);
+// }
+
+// console.log(add(1, 2, 4));
+
+
+//type alliases
+
+// type stringOrNum = string | number;
+// type objWithName = {name: string, uid: stringOrNum};
+
+// const logDetails = (uid: stringOrNum) => {
+//     console.log(uid);
+// }
+
+//function types
+//example
+
+// let greet: (a: string, b: string) => void;
+
+// greet = (name: string, greeting: string) => {
+//     console.log(`${greeting} ${name}`);
+// }
+
+// greet('mario', 'good morning');
+
+//example 2
+
+// let calc: (a:number, b: number, c: string) => number;
+
+// calc = (num1: number, num2: number, action: string) => {
+//     if (action === 'add') {
+//         return num1 + num2;
+//     }
+//     return num1 - num2;
+// }   //return num1 + num2;
+
+// console.log(calc(6, 2, 'add'));
+
+//example 3
+
+// let logDetails: (obj: {name: string, age: number}) => void;
+
+// type person = {name: string, age: number};
+
+// logDetails = (user: person) => {
+//     console.log(`${user.name} is ${user.age} years old`);
+// }
+
+// logDetails({
+//     name: 'Gift',
+//     age: 40
+// })
